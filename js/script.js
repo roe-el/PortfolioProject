8ec0bb4055b7ab72b5fff0e08efc0e1e32b1c8e8
roe-el <rogar28@gmail.com>
2017-09-14 17:21:17 -0500

    Change name of hovered-title to other-title.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	//Displaying text after click
-	$(document).on('click','.hovered-title', function(){
+	$(document).on('click','.other-title', function(){
 		$('.text1').css("display","none");
 		$(this).parent().children().eq(1).css("display","block");
 });
