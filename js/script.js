20b994a7946782201a82be29ed719d7ad0b07540
roe-el <rogar28@gmail.com>
2017-09-14 17:09:02 -0500

    Remove unneeded first function and add click function for Other section.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,7 @@
-$(function(){
-
-     //Close mobile nav on link click
-     
-
-     var navMobile = $('.navbar-collapse');
-
-     $('.navbar-collapse').on('click', 'a', null, function () {
-         $('.navbar-collapse').collapse('hide');
-     })
-    
+$(document).ready(function(){
+	//Displaying text after click
+	$(document).on('click','.hovered-title', function(){
+		$('.text1').css("display","none");
+		$(this).parent().children().eq(1).css("display","block");
+});
 });
